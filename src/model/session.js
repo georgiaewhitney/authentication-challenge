b556520605a91baa6a7ca567b0903a45f97caf92
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-01-27 19:03:40 +0000

    testing fixed, and challenge 1 pass

--- src/model/session.js
+++ src/model/session.js
@@ -1,9 +1,26 @@
 const db = require("../database/db.js");
+const crypto = require("node:crypto");
 
-const insert_session = db.prepare(`SELECT 1`);
+const insert_session = db.prepare(
+  /*sql*/
+  `
+INSERT INTO sessions (id, user_id, expires_at) VALUES (
+  $id, 
+  $user_id,
+  -- ensure expires_at column is set to future date
+  DATE('now', '+10 days')
+)
+`
+);
 
+// take user_id as an argument
 function createSession(user_id) {
-  // to-do
+  // generate a random string to use as session id
+  const id = crypto.randomBytes(18).toString("base64");
+  // insert new session into the db
+  insert_session.run({ id, user_id });
+  // return generated id to store in cookie later on
+  return id;
 }
 
 const select_session = db.prepare(`
