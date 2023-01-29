96c41695c218502babb0afaef5607faa02237792
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-01-29 14:23:51 +0000

    test 2 passed

--- src/routes/sign-up.js
+++ src/routes/sign-up.js
@@ -1,4 +1,8 @@
 const { Layout } = require("../templates.js");
+// use bcrypt library to hash password
+const bcrypt = require("bcryptjs");
+const { createUser } = require("../model/user.js");
+const { createSession } = require("../model/session.js");
 
 function get(req, res) {
   const title = "Create an account";
@@ -27,7 +31,22 @@ function post(req, res) {
   if (!email || !password) {
     res.status(400).send("Bad input");
   } else {
-    res.send("to-do");
+    // hash the password via bcrypt library
+    bcrypt.hash(password, 12).then((hash) => {
+      // use createUser func to insert new user in db
+      const user = createUser(email, hash);
+      // use createSession func to insert new session in db
+      const session_id = createSession(user.id);
+      // set signed sid cookie
+      res.cookie("sid", session_id, {
+        signed: true,
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+        sameSite: "lax",
+      });
+      // redirect to the new user's confession page
+      res.redirect(`/confessions/${user.id}`);
+    });
     /**
      * [1] Hash the password
      * [2] Create the user in the DB
